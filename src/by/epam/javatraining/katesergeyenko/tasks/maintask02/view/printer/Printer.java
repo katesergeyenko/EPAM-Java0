39673b32a8d1c22497f90235006d0aec5240a05e
katesergeyenko <31818732+katesergeyenko@users.noreply.github.com>
2019-01-25 11:16:42 +0300

    Delete Printer.java

--- src/by/epam/javatraining/katesergeyenko/tasks/maintask02/view/printer/Printer.java
+++
@@ -1,7 +0,0 @@
-package by.epam.javatraining.katesergeyenko.tasks.maintask02.view.printer;
-
-public class Printer {
-    public interface Printable {
-        public void printString(String string);
-    }
-}
